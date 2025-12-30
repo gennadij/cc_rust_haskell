c68a2300889a02d27900987b44475d1f5830363f
Gennadi Heimann <gennadiheimann@gmail.com>
2025-12-30 18:48:14 +0100

    feat: create dummy rest api server in c++

---
+++ cc/exact_square_root_rest_api_server/src/exact_square_root.cpp
@@ -0,0 +1,7 @@
+#include "exact_square_root.h"
+
+namespace exact_square_root {
+  Res ExactSquareRoot::GetExactSqrt(int readicand){
+    return Res{-1, -1};
+  }
+}
---
+++ cc/exact_square_root_rest_api_server/src/exact_square_root.h
@@ -0,0 +1,23 @@
+#ifndef EXACT_SQUARE_ROOT_H
+#define EXACT_SQUARE_ROOT_H
+
+
+namespace exact_square_root {
+
+struct Res{
+  int multiplicator{0};
+  int square_root{0};
+};
+
+class ExactSquareRoot {
+
+public:
+
+Res GetExactSqrt(int readicand);
+
+private:
+
+
+};
+}
+#endif // EXACT_SQUARE_ROOT_H
